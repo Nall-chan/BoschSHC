4097d3426d03621b95d37a800c5e0123c2fb2e2f
Michael Tröger <mt@neo-ami.de>
2023-04-12 23:25:28 +0200

    no message

---
+++ Bosch SmartHome Device/module.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+	class BoschSmartHomeDevice extends IPSModule
+	{
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			$this->RequireParent('{8D1D21A7-FDE3-EB16-B5B3-6D38D0673B62}');
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		public function Send()
+		{
+			$this->SendDataToParent(json_encode(['DataID' => '{FCC91718-B5E6-FD03-A393-7BAF6E4A7EEF}']));
+		}
+
+		public function ReceiveData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+		}
+	}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Bosch Smart Home
+
+Folgende Module beinhaltet das Bosch Smart Home Repository:
+
+- __Bosch SHC__ ([Dokumentation](Bosch%20SHC))  
+	Kurze Beschreibung des Moduls.
+
+- __Bosch SHC Configurator__ ([Dokumentation](Bosch%20SHC%20Configurator))  
+	Kurze Beschreibung des Moduls.
+
+- __Bosch SmartHome Device__ ([Dokumentation](Bosch%20SmartHome%20Device))  
+	Kurze Beschreibung des Moduls.
