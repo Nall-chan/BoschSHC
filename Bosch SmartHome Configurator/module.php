d0d4d8dfc2a9fa1e173b894064c607fbe7499bf1
Michael Tröger <mt@neo-ami.de>
2023-04-17 18:00:45 +0200

    no message

--- Bosch SmartHome Configurator/module.php
+++ Bosch SmartHome Configurator/module.php
@@ -2,31 +2,17 @@
 
 declare(strict_types=1);
 
-eval('declare(strict_types=1);namespace BoschSHCConf {?>' . file_get_contents(dirname(__DIR__) . '/libs/helper/BufferHelper.php') . '}');
 eval('declare(strict_types=1);namespace BoschSHCConf {?>' . file_get_contents(dirname(__DIR__) . '/libs/helper/DebugHelper.php') . '}');
-eval('declare(strict_types=1);namespace BoschSHCConf {?>' . file_get_contents(dirname(__DIR__) . '/libs/helper/ParentIOHelper.php') . '}');
 require_once dirname(__DIR__) . '/libs/SHCTypes.php';
-/**
- * @property int $ParentID
- */
     class BoschSmartHomeConfigurator extends IPSModule
     {
-        use \BoschSHCConf\BufferHelper,
-            \BoschSHCConf\DebugHelper,
-            \BoschSHCConf\InstanceStatus {
-            \BoschSHCConf\InstanceStatus::MessageSink as IOMessageSink; // MessageSink gibt es sowohl hier in der Klasse, als auch im Trait InstanceStatus. Hier wird für die Methode im Trait ein Alias benannt.
-            \BoschSHCConf\InstanceStatus::RegisterParent as IORegisterParent;
-            \BoschSHCConf\InstanceStatus::RequestAction as IORequestAction;
-            }
+        use            \BoschSHCConf\DebugHelper;
 
         public function Create()
         {
             //Never delete this line!
             parent::Create();
             $this->ConnectParent(\BoschSHC\GUID::IO);
-            if (IPS_GetKernelRunlevel() != KR_READY) {
-                $this->RegisterMessage(0, IPS_KERNELSTARTED);
-            }
         }
 
         public function Destroy()
@@ -39,36 +25,13 @@ require_once dirname(__DIR__) . '/libs/SHCTypes.php';
         {
             //Never delete this line!
             parent::ApplyChanges();
-            $this->RegisterMessage($this->InstanceID, FM_CONNECT);
-            $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
             $this->SetReceiveDataFilter('.*"DeviceId":"NOTHING".*');
-            if (IPS_GetKernelRunlevel() != KR_READY) {
-                return;
-            }
-            $this->RegisterParent();
         }
-        public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
-        {
-            $this->IOMessageSink($TimeStamp, $SenderID, $Message, $Data);
 
-            switch ($Message) {
-                case IPS_KERNELSTARTED:
-                    $this->UnregisterMessage(0, IPS_KERNELSTARTED);
-                    $this->KernelReady();
-                    break;
-            }
-        }
-        public function RequestAction($Ident, $Value)
-        {
-            if ($this->IORequestAction($Ident, $Value)) {
-                return true;
-            }
-            return false;
-        }
         public function GetConfigurationForm()
         {
             $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
-            if (!$this->HasActiveParent() || ($this->ParentID == 0)) {
+            if (!$this->HasActiveParent() || (IPS_GetInstance($this->InstanceID)['ConnectionID'] == 0)) {
                 $Form['actions'][] = [
                     'type'  => 'PopupAlert',
                     'popup' => [
@@ -90,31 +53,12 @@ require_once dirname(__DIR__) . '/libs/SHCTypes.php';
 
             return json_encode($Form);
         }
-        /**
-         * Wird ausgeführt wenn sich der Status vom Parent ändert.
-         */
-        protected function IOChangeState($State)
-        {
-            if ($State == IS_ACTIVE) {
-                $this->ReloadForm();
-            }
-        }
 
-        protected function KernelReady()
-        {
-            $this->RegisterParent();
-            if ($this->HasActiveParent()) {
-                $this->ApplyChanges();
-            }
-        }
-        protected function RegisterParent()
-        {
-            $this->IORegisterParent();
-        }
         protected function FilterInstances(int $InstanceID)
         {
             return IPS_GetInstance($InstanceID)['ConnectionID'] == IPS_GetInstance($this->InstanceID)['ConnectionID'];
         }
+
         protected function GetConfigParam(&$item1, $InstanceID, $ConfigParam)
         {
             $item1 = IPS_GetProperty($InstanceID, $ConfigParam);
@@ -140,6 +84,7 @@ require_once dirname(__DIR__) . '/libs/SHCTypes.php';
             $Values = json_decode($Result, true);
             return array_column($Values, 'name', 'id');
         }
+
         private function GetDevices()
         {
             $Rooms = $this->GetRooms();
@@ -194,7 +139,7 @@ require_once dirname(__DIR__) . '/libs/SHCTypes.php';
             return $Values;
         }
 
-        private function GetIPSInstances(): array
+        private function GetIPSInstances()
         {
             $InstanceIDList = array_filter(IPS_GetInstanceListByModuleID(\BoschSHC\GUID::Device), [$this, 'FilterInstances']);
             $InstanceIDList = array_flip(array_values($InstanceIDList));
